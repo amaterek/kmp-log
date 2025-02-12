04c616e52fb1b78cfd10bb609d9d2e92ce0cd78b
Arek M <17622543+amaterek@users.noreply.github.com>
2025-02-12 19:02:17 +0100

    Switch to java 17

--- log/build.gradle.kts
+++ log/build.gradle.kts
@@ -57,6 +57,11 @@ android {
         testImplementation(libs.junit)
         testImplementation(libs.mockk)
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 publishing {
